506dfeb5786533961554e04d97d4950021e10e9d
tiboCorb <corbrionthibault@gmail.com>
2020-10-14 10:13:59 +0200

    3 files modified

--- src/app/service/icon.service.ts
+++ src/app/service/icon.service.ts
@@ -1,19 +1,17 @@
-import { HttpClient } from '@angular/common/http'; 
-import { Injectable } from '@angular/core';
-import { Observable } from 'rxjs';
-import { Icon } from '../model/icon.model';
+import { HttpClient } from "@angular/common/http";
+import { Injectable } from "@angular/core";
+import { Observable } from "rxjs";
+import { Icon } from "../model/icon.model";
 
-
-@Injectable({providedIn:"root"})
+@Injectable({ providedIn: "root" })
 export class IconService {
-
   /**TODO */
 
-   constructor(private http: HttpClient) {
-     /**TODO */
-    }
+  constructor(private http: HttpClient) {
+    /**TODO */
+  }
 
-    public getIcon(): Observable<Icon[]> {
-        return this.http.get("../../../assets/icons.json") as Observable<Icon[]>;
-    }
+  public getIcon(): Observable<Icon[]> {
+    return this.http.get<Icon[]>("../../../assets/icons.json");
+  }
 }
