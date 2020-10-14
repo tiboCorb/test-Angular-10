fa0c581556c0f949253bcace027206759b6d53b1
tiboCorb <corbrionthibault@gmail.com>
2020-10-14 14:17:29 +0200

    1 file modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,20 +2,17 @@ import { Component, OnInit } from "@angular/core";
 import { Card } from "./model/card.model";
 import { Icon } from "./model/icon.model";
 
-
 @Component({
   selector: "my-app",
   templateUrl: "./app.component.html",
   styleUrls: ["./app.component.css"]
 })
 export class AppComponent implements OnInit {
-  public selectedIndex: number;
   public cards: Card[];
-  public selectedIcon : Icon;
-  public cardIndex : number;
+  public selectedIcon: Icon;
+  public cardIndex: number;
 
   constructor() {
-    this.selectedIndex = null;
     this.selectedIcon = null;
     this.cardIndex = null;
     this.cards = [];
@@ -27,7 +24,7 @@ export class AppComponent implements OnInit {
 
   select(index: number) {
     if (!this.cards[index].selected) {
-      this!.cards.forEach(card => (card.selected = false));
+      this.cards.forEach(card => (card.selected = false));
       this.cardIndex = index;
       this.cards[index].selected = true;
     } else {
@@ -40,15 +37,16 @@ export class AppComponent implements OnInit {
     /**TODO */
     if (this.cardIndex !== null) {
       const currentCard = this.cards[this.cardIndex];
-       this.cards[this.cardIndex] = {...currentCard,
-        icon : this.selectedIcon,
-        selected :false,
-       }
+      this.cards[this.cardIndex] = {
+        ...currentCard,
+        icon: this.selectedIcon,
+        selected: false
+      };
     }
   }
 
-  public selectionChanged(icon :Icon):void{
-    this.selectedIcon = {...icon}
+  public selectionChanged(icon: Icon): void {
+    this.selectedIcon = { ...icon };
   }
 
   private initCard() {
